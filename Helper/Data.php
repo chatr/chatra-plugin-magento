ce86b9085ec9acb6f504d0096f96a4080d517d8c
Petr Shardakov <dentist@onetwo.tv>
2021-03-04 23:54:18 +0700

    Bump version 1.0.1

--- Helper/Data.php
+++ Helper/Data.php
@@ -1,13 +1,14 @@
 <?php
+
 namespace Chatra\Livechat\Helper;
+
 use Magento\Framework\App\Helper\AbstractHelper;
 use Magento\Store\Model\ScopeInterface;
+
 class Data extends AbstractHelper
 {
-	public function getConfigValue($field)
-	{
-		return $this->scopeConfig->getValue(
-			$field, \Magento\Store\Model\ScopeInterface::SCOPE_STORE
-		);
-	}
+    public function getConfigValue($field)
+    {
+        return $this->scopeConfig->getValue($field, ScopeInterface::SCOPE_STORE);
+    }
 }
--- Observer/ConfigSaveObserver.php
+++ Observer/ConfigSaveObserver.php
@@ -1,33 +1,22 @@
 <?php
+
 namespace Chatra\Livechat\Observer;
 
 use Magento\Framework\Event\ObserverInterface;
-use Magento\Framework\Event\Observer as EventObserver;
-use Psr\Log\LoggerInterface as Logger;
+use Magento\Framework\Event\Observer;
 use Magento\Framework\App\Cache\Manager as CacheManager;
-use Magento\Framework\App\Cache\Type\Config as CacheTypeConfig;
 
 class ConfigSaveObserver implements ObserverInterface
 {
-    /**
-     * @var Logger
-     */
-    protected $logger;
-	protected $curDir;
+    protected $cacheManager;
 
-    /**
-     * @param Logger $logger
-     */
-    public function __construct(
-        Logger $logger,
-		\Magento\Framework\Filesystem\DirectoryList $dir
-    ) {
-        $this->logger = $logger;
-		$this->curDir = $dir;
+    public function __construct(CacheManager $cacheManager)
+    {
+        $this->cacheManager = $cacheManager;
     }
 
-    public function execute(EventObserver $observer)
+    public function execute(Observer $observer)
     {
-		$result = exec('/usr/bin/php '.$this->curDir->getRoot().'/bin/magento cache:clean config');
+        $this->cacheManager->clean($this->cacheManager->getAvailableTypes());
     }
 }
